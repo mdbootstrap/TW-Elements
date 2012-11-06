b409aa3ff344c1c6969bc65c0e5d6169dc99b8f7
Hyunje Alex Jun <me@noraesae.net>
2012-11-06 22:00:37 +0900

    Base source structure.

---
+++ src/perfect-scrollbar.js
@@ -0,0 +1,8 @@
+/* Copyright (c) 2012 HyeonJe Jun (http://github.com/noraesae)
+ * Licensed under the MIT License
+ */
+((function($) {
+    $.fn.perfectScrollbar = function() {
+        // TODO
+    };
+})(jQuery));
