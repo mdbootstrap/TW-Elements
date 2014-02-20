35aeb8db414393d01c2e8ae30fcccc494728e322
Steve Lamb <steve@stevelamb.io>
2014-02-20 10:06:30 -0800

    CommonJS compatibility

--- src/perfect-scrollbar.js
+++ src/perfect-scrollbar.js
@@ -6,6 +6,9 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node/CommonJS
+    factory(require('jquery'));
   } else {
     // Browser globals
     factory(jQuery);
