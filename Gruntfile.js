d24f9bd41b995c7c31c99cd8334bdf1bedc7c2f5
Hyunje Alex Jun <me@noraesae.net>
2014-10-31 02:15:36 +0000

    Use scss for stylesheet.
    We can build the scss source to .css and .min.css files with the
    `grunt build` command.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
 
   // Project configuration.
   grunt.initConfig({
@@ -38,15 +39,6 @@ module.exports = function (grunt) {
         src: 'src/perfect-scrollbar.js'
       }
     },
-    csslint: {
-      strict: {
-        options: {
-          csslintrc: '.csslintrc',
-          'import': 2
-        },
-        src: ['src/perfect-scrollbar.css']
-      }
-    },
     cssmin: {
       options: {
         banner: '<%= banner %>'
@@ -67,16 +59,15 @@ module.exports = function (grunt) {
         createTag: false,
         push: false
       }
+    },
+    sass: {
+      dist: {
+        files: {
+          'src/perfect-scrollbar.css': 'src/perfect-scrollbar.scss'
+        }
+      }
     }
   });
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-bump');
 
   grunt.registerTask('default', 'List commands', function () {
     grunt.log.writeln("");
@@ -85,12 +76,11 @@ module.exports = function (grunt) {
     grunt.log.writeln("Run 'grunt build' to minify the source files");
   });
 
-  grunt.registerTask('lint', ['jshint', 'csslint']);
-  grunt.registerTask('build', ['clean', 'uglify', 'cssmin']);
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('build', ['clean', 'uglify', 'sass', 'cssmin']);
   grunt.registerTask('travis', ['lint']);
   grunt.registerTask('release', 'Release a new version', function (arg) {
     var bumpType = arg ? ':' + arg : '';
     grunt.task.run(['lint', 'bump' + bumpType, 'build']);
   });
-
 };
