f75d187e45b0c0995d11d7f73139d71cf0b2be82
Hyunje Alex Jun <me@noraesae.net>
2015-01-15 23:59:52 +0000

    Separate main.js into some modules.
    Helper functions and the plugin function are separated.

---
+++ src/js/plugin/default-setting.js
@@ -0,0 +1,19 @@
+/* Copyright (c) 2015 Hyunje Alex Jun and other contributors
+ * Licensed under the MIT License
+ */
+'use strict';
+
+module.exports = {
+  wheelSpeed: 1,
+  wheelPropagation: false,
+  swipePropagation: true,
+  minScrollbarLength: null,
+  maxScrollbarLength: null,
+  useBothWheelAxes: false,
+  useKeyboard: true,
+  suppressScrollX: false,
+  suppressScrollY: false,
+  scrollXMarginOffset: 0,
+  scrollYMarginOffset: 0,
+  includePadding: false
+};
