0791c51505fcf717583a7c64838bbb5158d21197
Adam Miles <adammiles10@gmail.com>
2015-07-06 16:51:36 -0400

    Adding a null check on i not sure if this is the most optimal solution, but it did solve our error that we were encountering

--- src/js/plugin/destroy.js
+++ src/js/plugin/destroy.js
@@ -10,11 +10,14 @@ var d = require('../lib/dom')
 module.exports = function (element) {
   var i = instances.get(element);
 
-  i.event.unbindAll();
-  d.remove(i.scrollbarX);
-  d.remove(i.scrollbarY);
-  d.remove(i.scrollbarXRail);
-  d.remove(i.scrollbarYRail);
+  if (i){
+    i.event.unbindAll();
+    d.remove(i.scrollbarX);
+    d.remove(i.scrollbarY);
+    d.remove(i.scrollbarXRail);
+    d.remove(i.scrollbarYRail);
+  }
+
   h.removePsClasses(element);
 
   instances.remove(element);
