8761735a54e286f086a1afcc6200ecc7167bfb03
Hyunje Alex Jun <me@noraesae.net>
2015-10-03 21:16:23 +0900

    Update Travis CI and NPM configuration
    To be compatible with their newest versions.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@
 
 var gulp = require('gulp')
   , browserify = require('browserify')
+  , buffer = require('vinyl-buffer')
   , bump = require('gulp-bump')
   , connect = require('gulp-connect')
+  , del = require('del')
   , eslint = require('gulp-eslint')
   , insert = require('gulp-insert')
+  , path = require('path')
   , rename = require('gulp-rename')
-  , rimraf = require('gulp-rimraf')
   , sass = require('gulp-sass')
-  , transform = require('vinyl-transform')
+  , source = require('vinyl-source-stream')
+  , stream = require('event-stream')
   , uglify = require('gulp-uglify')
   , zip = require('gulp-zip');
 
@@ -23,63 +26,66 @@ gulp.task('lint', function () {
 });
 
 gulp.task('clean:js', function () {
-  return gulp.src('./dist/js/*.js', {read: false})
-    .pipe(rimraf());
+  return del(['./dist/js/*.js']);
 });
 
 gulp.task('clean:js:min', function () {
-  return gulp.src('./dist/js/min/*.js', {read: false})
-    .pipe(rimraf());
+  return del(['./dist/js/min/*.js']);
 });
 
-function browserified() {
-  return transform(function (filename) {
-    var b = browserify(filename);
-    return b.bundle();
-  });
-}
+var jsEntries = [
+  './src/js/adaptor/global.js',
+  './src/js/adaptor/jquery.js'
+];
 
 gulp.task('js', ['clean:js'], function () {
-  return gulp.src('./src/js/adaptor/*.js')
-    .pipe(browserified())
-    .pipe(insert.prepend(version))
-    .pipe(rename(function (path) {
-      if (path.basename === 'global') {
-        path.basename = 'perfect-scrollbar';
-      } else {
-        path.basename = 'perfect-scrollbar.' + path.basename;
-      }
-    }))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe(connect.reload());
+  var tasks = jsEntries.map(function (src) {
+    return browserify([src]).bundle()
+      .pipe(source(path.basename(src)))
+      .pipe(buffer())
+      .pipe(insert.prepend(version))
+      .pipe(rename(function (path) {
+        if (path.basename === 'global') {
+          path.basename = 'perfect-scrollbar';
+        } else {
+          path.basename = 'perfect-scrollbar.' + path.basename;
+        }
+      }))
+      .pipe(gulp.dest('./dist/js'))
+      .pipe(connect.reload());
+  });
+  return stream.merge.apply(null, tasks);
 });
 
 gulp.task('js:min', ['clean:js:min'], function () {
-  return gulp.src('./src/js/adaptor/*.js')
-    .pipe(browserified())
-    .pipe(uglify())
-    .pipe(insert.prepend(version))
-    .pipe(rename(function (path) {
-      if (path.basename === 'global') {
-        path.basename = 'perfect-scrollbar.min';
-      } else {
-        path.basename = 'perfect-scrollbar.' + path.basename + '.min';
-      }
-    }))
-    .pipe(gulp.dest('./dist/js/min'));
+  var tasks = jsEntries.map(function (src) {
+    return browserify([src]).bundle()
+      .pipe(source(path.basename(src)))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(insert.prepend(version))
+      .pipe(rename(function (path) {
+        if (path.basename === 'global') {
+          path.basename = 'perfect-scrollbar';
+        } else {
+          path.basename = 'perfect-scrollbar.' + path.basename;
+        }
+      }))
+      .pipe(gulp.dest('./dist/js/min'))
+      .pipe(connect.reload());
+  });
+  return stream.merge.apply(null, tasks);
 });
 
 gulp.task('clean:css', function () {
-  return gulp.src('./dist/css/perfect-scrollbar.css', {read: false})
-    .pipe(rimraf());
+  return del(['./dist/css/perfect-scrollbar.css']);
 });
 
 gulp.task('clean:css:min', function () {
-  return gulp.src('./dist/css/perfect-scrollbar.min.css', {read: false})
-    .pipe(rimraf());
+  return del(['./dist/css/perfect-scrollbar.min.css']);
 });
 
-gulp.task('sass', ['clean:css'], function () {
+gulp.task('css', ['clean:css'], function () {
   return gulp.src('./src/css/main.scss')
     .pipe(sass())
     .pipe(insert.prepend(version))
@@ -88,7 +94,7 @@ gulp.task('sass', ['clean:css'], function () {
     .pipe(connect.reload());
 });
 
-gulp.task('sass:min', ['clean:css:min'], function () {
+gulp.task('css:min', ['clean:css:min'], function () {
   return gulp.src('./src/css/main.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(insert.prepend(version))
@@ -114,7 +120,7 @@ gulp.task('bump', function () {
 
 gulp.task('release', ['bump', 'build']);
 
-gulp.task('build', ['js', 'js:min', 'sass', 'sass:min']);
+gulp.task('build', ['js', 'js:min', 'css', 'css:min']);
 
 gulp.task('connect', ['build'], function () {
   connect.server({
@@ -125,7 +131,7 @@ gulp.task('connect', ['build'], function () {
 
 gulp.task('watch', function () {
   gulp.watch(['src/js/**/*'], ['js']);
-  gulp.watch(['src/css/**/*'], ['sass']);
+  gulp.watch(['src/css/**/*'], ['css']);
 });
 
 gulp.task('serve', ['connect', 'watch']);
