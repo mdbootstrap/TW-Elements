452dbdb7a44ea9f9f94e78db9b97ddf0fe62ec82
Hyunje Jun <me@noraesae.net>
2017-09-01 18:38:55 +0900

    Use null instead of undefined for options

--- src/options.ts
+++ src/options.ts
@@ -1,20 +1,29 @@
 export interface PerfectScrollbarOptions {
-  handlers: Array<'click-rail' | 'drag-scrollbar' | 'keyboard' | 'wheel' | 'touch' | 'selection'>,
-  maxScrollbarLength?: number,
-  minScrollbarLength?: number,
-  scrollXMarginOffset: number,
-  scrollYMarginOffset: number,
-  suppressScrollX: boolean,
-  suppressScrollY: boolean,
-  swipePropagation: boolean,
-  swipeEasing: boolean,
-  useBothWheelAxes: boolean,
-  wheelPropagation: boolean,
-  wheelSpeed: number,
+  handlers: Array<
+    | 'click-rail'
+    | 'drag-scrollbar'
+    | 'keyboard'
+    | 'wheel'
+    | 'touch'
+    | 'selection'
+  >;
+  maxScrollbarLength: number | null;
+  minScrollbarLength: number | null;
+  scrollXMarginOffset: number;
+  scrollYMarginOffset: number;
+  suppressScrollX: boolean;
+  suppressScrollY: boolean;
+  swipePropagation: boolean;
+  swipeEasing: boolean;
+  useBothWheelAxes: boolean;
+  wheelPropagation: boolean;
+  wheelSpeed: number;
 }
 
 export const defaultOptions = (): PerfectScrollbarOptions => ({
   handlers: ['click-rail', 'drag-scrollbar', 'keyboard', 'wheel', 'touch'],
+  maxScrollbarLength: null,
+  minScrollbarLength: null,
   scrollXMarginOffset: 0,
   scrollYMarginOffset: 0,
   suppressScrollX: false,
