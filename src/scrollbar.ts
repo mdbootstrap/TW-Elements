47f2af98b163373a1b3d7b46dcf16faef3c0a6aa
Hyunje Jun <me@noraesae.net>
2017-09-04 17:32:20 +0900

    Add scrollbar module

---
+++ src/scrollbar.ts
@@ -0,0 +1,25 @@
+import { remove } from './lib/dom';
+
+export class Scrollbar {
+  railEl: HTMLDivElement = document.createElement('div');
+  thumbEl: HTMLDivElement = document.createElement('div');
+
+  constructor(public axis: 'x' | 'y') {
+    this.railEl.className = `ps_scrollbar--${this.axis}-rail`;
+    this.thumbEl.className = `ps_scrollbar--${this.axis}`;
+  }
+
+  isInstalledAt(container: HTMLElement): boolean {
+    return this.railEl.parentElement === container;
+  }
+
+  appendTo(container: HTMLElement) {
+    this.railEl.appendChild(this.thumbEl);
+    container.appendChild(this.railEl);
+  }
+
+  destroy() {
+    remove(this.thumbEl);
+    remove(this.railEl);
+  }
+}
