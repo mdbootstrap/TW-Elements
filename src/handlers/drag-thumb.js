78b4cb0d65fc8ac83b39b0b8217685a26bcaf556
Hyunje Jun <me@noraesae.net>
2017-10-10 11:34:25 +0900

    Make logic common for axes in drag-thumb

--- src/handlers/drag-thumb.js
+++ src/handlers/drag-thumb.js
@@ -5,92 +5,76 @@ import updateScroll from '../update-scroll';
 import { toInt } from '../lib/util';
 
 export default function(i) {
-  bindMouseScrollXHandler(i);
-  bindMouseScrollYHandler(i);
+  bindMouseScrollHandler(i, [
+    'containerWidth',
+    'contentWidth',
+    'pageX',
+    'railXWidth',
+    'railXRatio',
+    'scrollbarX',
+    'scrollbarXWidth',
+    'scrollbarXRail',
+    'scrollbarXLeft',
+    'left',
+  ]);
+  bindMouseScrollHandler(i, [
+    'containerHeight',
+    'contentHeight',
+    'pageY',
+    'railYHeight',
+    'railYRatio',
+    'scrollbarY',
+    'scrollbarYHeight',
+    'scrollbarYRail',
+    'scrollbarYTop',
+    'top',
+  ]);
 }
 
-function bindMouseScrollXHandler(i) {
-  const element = i.element;
-
-  let currentLeft = null;
-  let currentPageX = null;
-
-  function updateScrollLeft(deltaX) {
-    const newLeft = currentLeft + deltaX * i.railXRatio;
-    const maxLeft =
-      Math.max(0, i.scrollbarXRail.getBoundingClientRect().left) +
-      i.railXRatio * (i.railXWidth - i.scrollbarXWidth);
-
-    if (newLeft < 0) {
-      i.scrollbarXLeft = 0;
-    } else if (newLeft > maxLeft) {
-      i.scrollbarXLeft = maxLeft;
-    } else {
-      i.scrollbarXLeft = newLeft;
-    }
-
-    const scrollLeft =
-      toInt(
-        i.scrollbarXLeft *
-          (i.contentWidth - i.containerWidth) /
-          (i.containerWidth - i.railXRatio * i.scrollbarXWidth)
-      ) - i.negativeScrollAdjustment;
-    updateScroll(i, 'left', scrollLeft);
-  }
-
-  function mouseMoveHandler(e) {
-    updateScrollLeft(e.pageX - currentPageX);
-    updateGeometry(i);
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  function mouseUpHandler() {
-    i.event.unbind(i.ownerDocument, 'mousemove', mouseMoveHandler);
-  }
-
-  i.event.bind(i.scrollbarX, 'mousedown', e => {
-    currentPageX = e.pageX;
-    currentLeft = toInt(CSS.get(i.scrollbarX).left) * i.railXRatio;
-
-    i.event.bind(i.ownerDocument, 'mousemove', mouseMoveHandler);
-    i.event.once(i.ownerDocument, 'mouseup', mouseUpHandler);
-
-    e.stopPropagation();
-    e.preventDefault();
-  });
-}
-
-function bindMouseScrollYHandler(i) {
+function bindMouseScrollHandler(
+  i,
+  [
+    containerHeight,
+    contentHeight,
+    pageY,
+    railYHeight,
+    railYRatio,
+    scrollbarY,
+    scrollbarYHeight,
+    scrollbarYRail,
+    scrollbarYTop,
+    top,
+  ]
+) {
   const element = i.element;
 
   let currentTop = null;
   let currentPageY = null;
 
   function updateScrollTop(deltaY) {
-    const newTop = currentTop + deltaY * i.railYRatio;
+    const newTop = currentTop + deltaY * i[railYRatio];
     const maxTop =
-      Math.max(0, i.scrollbarYRail.getBoundingClientRect().top) +
-      i.railYRatio * (i.railYHeight - i.scrollbarYHeight);
+      Math.max(0, i[scrollbarYRail].getBoundingClientRect()[top]) +
+      i[railYRatio] * (i[railYHeight] - i[scrollbarYHeight]);
 
     if (newTop < 0) {
-      i.scrollbarYTop = 0;
+      i[scrollbarYTop] = 0;
     } else if (newTop > maxTop) {
-      i.scrollbarYTop = maxTop;
+      i[scrollbarYTop] = maxTop;
     } else {
-      i.scrollbarYTop = newTop;
+      i[scrollbarYTop] = newTop;
     }
 
     const scrollTop = toInt(
-      i.scrollbarYTop *
-        (i.contentHeight - i.containerHeight) /
-        (i.containerHeight - i.railYRatio * i.scrollbarYHeight)
+      i[scrollbarYTop] *
+        (i[contentHeight] - i[containerHeight]) /
+        (i[containerHeight] - i[railYRatio] * i[scrollbarYHeight])
     );
-    updateScroll(i, 'top', scrollTop);
+    updateScroll(i, top, scrollTop);
   }
 
   function mouseMoveHandler(e) {
-    updateScrollTop(e.pageY - currentPageY);
+    updateScrollTop(e[pageY] - currentPageY);
     updateGeometry(i);
     e.stopPropagation();
     e.preventDefault();
@@ -100,9 +84,9 @@ function bindMouseScrollYHandler(i) {
     i.event.unbind(i.ownerDocument, 'mousemove', mouseMoveHandler);
   }
 
-  i.event.bind(i.scrollbarY, 'mousedown', e => {
-    currentPageY = e.pageY;
-    currentTop = toInt(CSS.get(i.scrollbarY).top) * i.railYRatio;
+  i.event.bind(i[scrollbarY], 'mousedown', e => {
+    currentPageY = e[pageY];
+    currentTop = toInt(CSS.get(i[scrollbarY])[top]) * i[railYRatio];
 
     i.event.bind(i.ownerDocument, 'mousemove', mouseMoveHandler);
     i.event.once(i.ownerDocument, 'mouseup', mouseUpHandler);
