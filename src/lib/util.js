6552bfb9222d82eb6da4600b165012381b577cd0
zalesky <zal3sky@gmail.com>
2018-11-07 17:17:32 +0200

    Update util.js

--- src/lib/util.js
+++ src/lib/util.js
@@ -31,7 +31,8 @@ export const env = {
     'WebkitAppearance' in document.documentElement.style,
   supportsTouch:
     typeof window !== 'undefined' &&
-    ('ontouchstart' in window || navigator.maxTouchPoints > 0 ||
+    ('ontouchstart' in window || 
+      navigator.maxTouchPoints > 0 ||
       (window.DocumentTouch && document instanceof window.DocumentTouch)),
   supportsIePointer:
     typeof navigator !== 'undefined' && navigator.msMaxTouchPoints,
