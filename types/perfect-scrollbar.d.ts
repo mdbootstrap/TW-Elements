12553620bc880c8d941f8c7954261ff96e9a014f
awei.yu <328080339@qq.com>
2018-12-11 11:01:24 +0800

    Update perfect-scrollbar.d.ts
    Better compatibility with parent class Element instead of HTMLElement

--- types/perfect-scrollbar.d.ts
+++ types/perfect-scrollbar.d.ts
@@ -16,7 +16,7 @@ declare namespace PerfectScrollbar {
 }
 
 declare class PerfectScrollbar {
-  constructor(element: string | HTMLElement, options?: PerfectScrollbar.Options);
+  constructor(element: string | Element, options?: PerfectScrollbar.Options);
 
   update(): void;
   destroy(): void;
