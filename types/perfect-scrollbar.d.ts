5e57df2e7f008b655b460a317da7cbfcd729fd14
Samir Syed <syed.samiruddin@gmail.com>
2019-07-18 15:19:04 +0530

    Added missing properties in type definition (#808)

--- types/perfect-scrollbar.d.ts
+++ types/perfect-scrollbar.d.ts
@@ -21,6 +21,41 @@ declare class PerfectScrollbar {
   update(): void;
   destroy(): void;
 
+  containerHeight: number;
+  containerWidth: number;
+  contentHeight: number;
+  contentWidth: number;
+  element: HTMLElement;
+  isAlive: boolean;
+  isNegativeScroll: boolean;
+  isRtl: boolean;
+  isScrollbarXUsingBottom: boolean;
+  isScrollbarYUsingBottom: boolean;
+  lastScrollLeft: boolean;
+  lastScrollTop: boolean;
+  negativeScrollAdjustment: number;
+  railBorderXWidth: number;
+  railBorderYWidth: number;
+  railXMarginWidth: number;
+  railXRatio: number;
+  railXWidth: number;
+  railYHeight: number;
+  railYMarginHeight: number;
+  railYRatio: number;
+  scrollbarX: HTMLElement;
+  scrollbarXActive: boolean;
+  scrollbarXBottom: number;
+  scrollbarXLeft: number;
+  scrollbarXRail: HTMLElement;
+  scrollbarXWidth: number;
+  scrollbarY: HTMLElement;
+  scrollbarYActive: boolean;
+  scrollbarYHeight: number;
+  scrollbarYOuterWidth?: number;
+  scrollbarYRail: HTMLElement;
+  scrollbarYRight: number;
+  scrollbarYTop: number;
+  settings: PerfectScrollbar.Options;
   reach: { x: 'start' | 'end' | null, y: 'start' | 'end' | null };
 }
 
