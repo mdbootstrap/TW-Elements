8c0c27653540a6c6adeee9dece522a76d474fa06
Dawid Adach <d.adach@mdbootstrap.com>
2021-05-10 14:13:58 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Tailwind-Elements
+# tw
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["layouts/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
