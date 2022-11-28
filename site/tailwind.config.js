a32ce983889bc48eb64a8514fe35f8ff78952ac0
Jacek Roszak <amigo9569@gmail.com>
2022-11-28 04:36:12 +0100

    feat(carousel): remove static bs classes

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.html', './static/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        'grow': 'spinnerGrow 0.75s linear infinite',
+        'grow-slow':'spinnerGrow 1.5s linear infinite',
+        'spin-slow':'spin 1.5s linear infinite',
+      },
+      keyframes:{
+        spinnerGrow:{
+          '0%':{transform: 'scale(0)'},
+          '50%':{transform: 'none', opacity: '1'},
+        },
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar'), require('./static/js/plugin')],
 };
