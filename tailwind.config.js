ab20a72021628f84cb0fbfaa8b9612aafd865e6d
Marta Szymanska <m.szymanska@mdbootstrap.com>
2022-11-30 15:13:24 +0100

    Fix placeholders

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   content: ['./demo/**/*.{html,js}', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        placeholderGlow: {
+          '50%': { opacity: '0.2' },
+        },
+        placeholderWave: {
+          '100%': { maskPosition: '-200% 0%' },
+        },
+      },
+    },
   },
   plugins: [require('./src/js/plugin')],
-  darkMode: 'class',
+  // darkMode: 'class',
 };
