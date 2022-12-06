bc5dd1934c00f12ccb0f995d5e4420ae960a035c
Jacek Roszak <amigo9569@gmail.com>
2022-12-06 12:14:17 +0100

    (config): remove keyframes and animations from config

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,19 +1,7 @@
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.html', './static/**/*.js'],
   theme: {
-    extend: {
-      animation:{
-        'grow': 'spinnerGrow 0.75s linear infinite',
-        'grow-slow':'spinnerGrow 1.5s linear infinite',
-        'spin-slow':'spin 1.5s linear infinite',
-      },
-      keyframes:{
-        spinnerGrow:{
-          '0%':{transform: 'scale(0)'},
-          '50%':{transform: 'none', opacity: '1'},
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [require('tailwind-scrollbar'), require('./static/js/plugin')],
 };
