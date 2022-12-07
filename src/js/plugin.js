9015efa29975e2ab3537f6f63dbd0997a755dd7c
Trochonovitz <mat.trochonowicz@gmail.com>
2022-12-07 09:34:45 +0100

    Animations: removed duplicated animation

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -236,17 +236,6 @@ module.exports = plugin(
               transform: 'scale3d(1, 1, 1)',
             },
           },
-          pulse: {
-            '0%': {
-              transform: 'scale3d(1, 1, 1)',
-            },
-            '50%': {
-              transform: 'scale3d(1.05, 1.05, 1.05)',
-            },
-            '100%': {
-              transform: 'scale3d(1, 1, 1)',
-            },
-          },
         },
       },
     },
