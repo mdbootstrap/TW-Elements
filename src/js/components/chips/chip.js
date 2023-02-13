708a72e7967f9926b0d2baf467d4adedd0430805
Mateusz <43219043+Trochonovitz@users.noreply.github.com>
2023-02-13 15:02:08 +0100

    Icons migration (#1310)
    * Accordion, V/Carousel, Video: icons migration
    
    * Modal, Gallery, Offcanvas, Toast: icons migration
    
    * Alerts, Btns, Chips, Tesimonial: icons migration
    
    * Dividers, Dropdown, Headers: icons migration
    
    * move demo sites to new directories
    
    * Icons: icons migrations
    
    * Jumbotron, Login, Noti, Rating, ScrollToTop: icons
    
    * Animation, Testimonials (stars), Timeline: icons
    
    * Input group, search, footer: icons migration
    
    * Mega menu, Navbar, Sidenav, Contribute: icons
    
    * Social buttons: icons migration
    
    * Sidenav exampels: icons migration
    
    * Date/timepicker: icons migration
    
    * Main page, sidenav, header, theme switcher: icons
    
    * Icons migrations in shared request notification
    
    * Accordion: fix chevron animation position
    
    ---------
    
    Co-authored-by: Bartosz Cylwik <bartosz.cylwik94@gmail.com>
    Co-authored-by: Mikolaj Smolenski <smolenski.mikolaj@gmail.com>

--- src/js/components/chips/chip.js
+++ src/js/components/chips/chip.js
@@ -23,8 +23,7 @@ const ATTR_SELECTOR_CHIP_CLOSE = `[${ATTR_CHIP_CLOSE}]`;
 const EVENT_DELETE = "delete.te.chips";
 const EVENT_SELECT = "select.te.chip";
 
-const defaultIcon =
-  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>';
+const defaultIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>`;
 
 const DefaultType = {
   text: "string",
