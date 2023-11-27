d21c5543a3650b3762a95dba1388943680e28c09
Trochonovitz <mat.trochonowicz@gmail.com>
2023-11-27 16:49:49 +0100

    Select: remove unnecessary arg

--- src/js/forms/select/index.js
+++ src/js/forms/select/index.js
@@ -536,7 +536,6 @@ class Select {
       this._config,
       this._label,
       this._classes,
-      this._element.id,
       this._config.customArrow
     );
     this._element.parentNode.insertBefore(template, this._element);
