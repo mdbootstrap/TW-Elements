afc09cbcb2c945f17cdb4227c7ef3ad41ce24bd7
Bartosz Cylwik <52455076+juujisai@users.noreply.github.com>
2024-01-15 15:08:29 +0100

    Buttons, Button group, Input - Update theming (#2286)
    * Buttons - update theming
    
    * Button group - update theming
    
    * Input - update theming

--- src/js/forms/input.js
+++ src/js/forms/input.js
@@ -59,21 +59,21 @@ export const DefaultClasses = {
   notchLeading:
     "pointer-events-none border border-solid box-border bg-transparent transition-all duration-200 ease-linear motion-reduce:transition-none left-0 top-0 h-full w-2 border-r-0 rounded-l-[0.25rem] group-data-[te-input-focused]:border-r-0 group-data-[te-input-state-active]:border-r-0",
   notchLeadingNormal:
-    "border-neutral-300 dark:border-neutral-600 group-data-[te-input-focused]:shadow-[-1px_0_0_#3b71ca,_0_1px_0_0_#3b71ca,_0_-1px_0_0_#3b71ca] group-data-[te-input-focused]:border-primary",
+    "border-secondary-500 dark:border-neutral-400 group-data-[te-input-focused]:shadow-notch-1 group-data-[te-input-focused]:border-primary",
   notchLeadingWhite:
-    "border-neutral-200 group-data-[te-input-focused]:shadow-[-1px_0_0_#ffffff,_0_1px_0_0_#ffffff,_0_-1px_0_0_#ffffff] group-data-[te-input-focused]:border-white",
+    "border-neutral-200 group-data-[te-input-focused]:shadow-notch-1 group-data-[te-input-focused]:shadow-white group-data-[te-input-focused]:border-white",
   notchMiddle:
     "pointer-events-none border border-solid box-border bg-transparent transition-all duration-200 ease-linear motion-reduce:transition-none grow-0 shrink-0 basis-auto w-auto max-w-[calc(100%-1rem)] h-full border-r-0 border-l-0 group-data-[te-input-focused]:border-x-0 group-data-[te-input-state-active]:border-x-0 group-data-[te-input-focused]:border-t group-data-[te-input-state-active]:border-t group-data-[te-input-focused]:border-solid group-data-[te-input-state-active]:border-solid group-data-[te-input-focused]:border-t-transparent group-data-[te-input-state-active]:border-t-transparent",
   notchMiddleNormal:
-    "border-neutral-300 dark:border-neutral-600 group-data-[te-input-focused]:shadow-[0_1px_0_0_#3b71ca] group-data-[te-input-focused]:border-primary",
+    "border-secondary-500 dark:border-neutral-400 group-data-[te-input-focused]:shadow-notch-2 group-data-[te-input-focused]:border-primary",
   notchMiddleWhite:
-    "border-neutral-200 group-data-[te-input-focused]:shadow-[0_1px_0_0_#ffffff] group-data-[te-input-focused]:border-white",
+    "border-neutral-200 group-data-[te-input-focused]:shadow-notch-2 group-data-[te-input-focused]:shadow-white group-data-[te-input-focused]:border-white",
   notchTrailing:
     "pointer-events-none border border-solid box-border bg-transparent transition-all duration-200 ease-linear motion-reduce:transition-none grow h-full border-l-0 rounded-r-[0.25rem] group-data-[te-input-focused]:border-l-0 group-data-[te-input-state-active]:border-l-0",
   notchTrailingNormal:
-    "border-neutral-300 dark:border-neutral-600 group-data-[te-input-focused]:shadow-[1px_0_0_#3b71ca,_0_-1px_0_0_#3b71ca,_0_1px_0_0_#3b71ca] group-data-[te-input-focused]:border-primary",
+    "border-secondary-500 dark:border-neutral-400 group-data-[te-input-focused]:shadow-notch-3 group-data-[te-input-focused]:border-primary",
   notchTrailingWhite:
-    "border-neutral-200 group-data-[te-input-focused]:shadow-[1px_0_0_#ffffff,_0_-1px_0_0_#ffffff,_0_1px_0_0_#ffffff] group-data-[te-input-focused]:border-white",
+    "border-neutral-200 group-data-[te-input-focused]:shadow-notch-3 group-data-[te-input-focused]:shadow-white group-data-[te-input-focused]:border-white",
   counter: "text-right leading-[1.6]",
 };
 
